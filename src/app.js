73e689c298cd1ca69106a30f36e6056f962c5eda
Andrio Yuli Wijaya <andrio@uas.ac.id>
2026-03-13 14:43:34 +0700

    feat: setup database connection

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+require('./database/db')
 
 const app = express();
 const PORT = process.env.PORT || 5001;
